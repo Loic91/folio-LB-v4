69c7af27efea831673dbef2d5ec14ebabc4cc388
Loic91 <62215559+Loic91@users.noreply.github.com>
2021-07-18 19:13:53 +0200

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,51 @@
+gsap.from('.word-one', 1.9, {
+    y: '100%',
+    delay: 5.9
+})
+gsap.from('.word-two', 1.9, {
+    y: '100%',
+    // delay: 6
+    delay: 5.95
+})
+gsap.from('.word-three', 1.9, {
+    y: '100%',
+    // delay: 6.1
+    delay: 6
+})
+gsap.from('.word-four', 1.9, {
+    y: '100%',
+    // delay: 6.2
+    delay: 6.05
+})
+gsap.from('.word-five', 1.9, {
+    y: '100%',
+    // delay: 6.3
+    delay: 6.1
+})
+
+
+gsap.from('.word-one-2', 1.9, {
+    y: '100%',
+    // delay: 6.5
+    delay: 6.2
+})
+gsap.from('.word-two-2', 1.9, {
+    y: '100%',
+    // delay: 6.6
+    delay: 6.25
+})
+gsap.from('.word-three-2', 1.9, {
+    y: '100%',
+    // delay: 6.7
+    delay: 6.3
+})
+gsap.from('.word-four-2', 1.9, {
+    y: '100%',
+    // delay: 6.8
+    delay: 6.35
+})
+gsap.from('.word-five-2', 1.9, {
+    y: '100%',
+    // delay: 6.9
+    delay: 6.4
+})
