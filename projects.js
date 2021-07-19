6f99f6a8a752a20b91ae2ad4862dd363b7ee45ed
Loic91 <62215559+Loic91@users.noreply.github.com>
2021-07-20 00:04:00 +0200

    Add files via upload

--- projects.js
+++ projects.js
@@ -1,76 +1,76 @@
 gsap.from('.word-one', 1.9, {
     y: '100%',
-    delay: 1.9
+    delay: 0.9
 })
 gsap.from('.word-two', 1.9, {
     y: '100%',
     // delay: 6
-    delay: 1.95
+    delay: 0.95
 })
 gsap.from('.word-three', 1.9, {
     y: '100%',
     // delay: 6.1
-    delay: 2
+    delay: 1
 })
 gsap.from('.word-four', 1.9, {
     y: '100%',
     // delay: 6.2
-    delay: 2.05
+    delay: 1.05
 })
 gsap.from('.word-five', 1.9, {
     y: '100%',
     // delay: 6.3
-    delay: 2.1
+    delay: 1.15
 })
 gsap.from('.word-six', 1.9, {
     y: '100%',
     // delay: 6.3
-    delay: 2.2
+    delay: 1.2
 })
 gsap.from('.word-seven', 1.9, {
     y: '100%',
     // delay: 6.3
-    delay: 2.3
+    delay: 1.25
 })
 
 
 gsap.from('.word-one-2', 1.9, {
     y: '100%',
     // delay: 6.5
-    delay: 2.4
+    delay: 1.3
 })
 gsap.from('.word-two-2', 1.9, {
     y: '100%',
     // delay: 6.6
-    delay: 2.45
+    delay: 1.35
 })
 gsap.from('.word-three-2', 1.9, {
     y: '100%',
     // delay: 6.7
-    delay: 2.5
+    delay: 1.4
 })
 gsap.from('.word-four-2', 1.9, {
     y: '100%',
     // delay: 6.8
-    delay: 2.55
+    delay: 1.45
 })
 gsap.from('.word-five-2', 1.9, {
     y: '100%',
     // delay: 6.9
-    delay: 2.6
+    delay: 1.5
 })
 gsap.from('.word-six-2', 1.9, {
     y: '100%',
     // delay: 6.9
-    delay: 2.65
+    delay: 1.55
 })
 gsap.from('.word-seven-2', 1.9, {
     y: '100%',
     // delay: 6.9
-    delay: 2.7
+    delay: 1.6
 })
 gsap.from('.word-eight-2', 1.9, {
     y: '100%',
     // delay: 6.9
-    delay: 2.75
+    delay: 1.65
 })
