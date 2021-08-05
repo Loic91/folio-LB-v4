67c1b6824982609558a50a06c2babc729e1ffe2e
Loic91 <62215559+Loic91@users.noreply.github.com>
2021-08-05 02:47:02 +0200

    Add files via upload

--- projects.js
+++ projects.js
@@ -41,6 +41,21 @@ gsap.from('.word-one-0', 1.9, {
     ease: Expo.easeInOut,
     delay: 0.9,
 })
+gsap.from('.word-one-0_mobile', 1.9, {
+    // y: '100%',
+    // delay: 0.9
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: 0.9,
+})
+gsap.from('.word-one-0_tab', 1.9, {
+    // y: '100%',
+    // delay: 0.9
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: 0.9,
+})
+
 gsap.from('.word-two-0', 1.9, {
     // y: '100%',
     // delay: 0.93
@@ -48,7 +63,36 @@ gsap.from('.word-two-0', 1.9, {
     ease: Expo.easeInOut,
     delay: 0.6,
 })
+gsap.from('.word-two-0_mobile', 1.9, {
+    // y: '100%',
+    // delay: 0.93
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: 0.6,
+})
+gsap.from('.word-two-0_tab', 1.9, {
+    // y: '100%',
+    // delay: 0.93
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: 0.6,
+})
+
 gsap.from('.word-three-0', 1.9, {
+    // y: '100%',
+    // delay: 0.96
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: 1.3,
+})
+gsap.from('.word-three-0_mobile', 1.9, {
+    // y: '100%',
+    // delay: 0.96
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: 1.3,
+})
+gsap.from('.word-three-0_tab', 1.9, {
     // y: '100%',
     // delay: 0.96
     opacity: 0,
@@ -65,6 +109,21 @@ gsap.from('.word-one-02', 1.9, {
     ease: Expo.easeInOut,
     delay: 0.7,
 })
+gsap.from('.word-one-02_mobile', 1.9, {
+    // y: '100%',
+    // delay: 0.99
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: 0.7,
+})
+gsap.from('.word-one-02_tab', 1.9, {
+    // y: '100%',
+    // delay: 0.99
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: 0.7,
+})
+
 gsap.from('.word-two-02', 1.9, {
     // y: '100%',
     // delay: 1.02
@@ -72,6 +131,21 @@ gsap.from('.word-two-02', 1.9, {
     ease: Expo.easeInOut,
     delay: 1,
 })
+gsap.from('.word-two-02_mobile', 1.9, {
+    // y: '100%',
+    // delay: 1.02
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: 1,
+})
+gsap.from('.word-two-02_tab', 1.9, {
+    // y: '100%',
+    // delay: 1.02
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: 1,
+})
+
 gsap.from('.word-three-02', 1.9, {
     // y: '100%',
     // delay: 1.05
@@ -79,6 +153,21 @@ gsap.from('.word-three-02', 1.9, {
     ease: Expo.easeInOut,
     delay: 0.5,
 })
+gsap.from('.word-three-02_mobile', 1.9, {
+    // y: '100%',
+    // delay: 1.05
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: 0.5,
+})
+gsap.from('.word-three-02_tab', 1.9, {
+    // y: '100%',
+    // delay: 1.05
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: 0.5,
+})
+
 gsap.from('.word-four-02', 1.9, {
     // y: '100%',
     // delay: 1.08
@@ -86,6 +175,21 @@ gsap.from('.word-four-02', 1.9, {
     ease: Expo.easeInOut,
     delay: 1.4,
 })
+gsap.from('.word-four-02_mobile', 1.9, {
+    // y: '100%',
+    // delay: 1.08
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: 1.4,
+})
+gsap.from('.word-four-02_tab', 1.9, {
+    // y: '100%',
+    // delay: 1.08
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: 1.4,
+})
+
 gsap.from('.word-five-02', 1.9, {
     // y: '100%',
     // delay: 1.11
@@ -93,6 +197,21 @@ gsap.from('.word-five-02', 1.9, {
     ease: Expo.easeInOut,
     delay: 0.9,
 })
+gsap.from('.word-five-02_mobile', 1.9, {
+    // y: '100%',
+    // delay: 1.11
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: 0.9,
+})
+gsap.from('.word-five-02_tab', 1.9, {
+    // y: '100%',
+    // delay: 1.11
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: 0.9,
+})
+
 gsap.from('.word-six-02', 1.9, {
     // y: '100%',
     // delay: 1.14
@@ -100,7 +219,36 @@ gsap.from('.word-six-02', 1.9, {
     ease: Expo.easeInOut,
     delay: 1.2,
 })
+gsap.from('.word-six-02_mobile', 1.9, {
+    // y: '100%',
+    // delay: 1.14
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: 1.2,
+})
+gsap.from('.word-six-02_tab', 1.9, {
+    // y: '100%',
+    // delay: 1.14
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: 1.2,
+})
+
 gsap.from('.word-seven-02', 1.9, {
+    // y: '100%',
+    // delay: 1.17
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: 0.6,
+})
+gsap.from('.word-seven-02_mobile', 1.9, {
+    // y: '100%',
+    // delay: 1.17
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: 0.6,
+})
+gsap.from('.word-seven-02_tab', 1.9, {
     // y: '100%',
     // delay: 1.17
     opacity: 0,
@@ -115,6 +263,54 @@ gsap.from('.fleche', 5.5, {
     ease: Expo.easeInOut,
     delay: 1.4,
 })
+
+
+// ANIMATION NAME *********************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************
+
+TweenMax.to(".t0", 1.9, {
+    x: "-170",
+    y: "-30",
+    scale: "0.6",
+    ease: Expo.easeInOut,
+    delay: 2.9,
+});
+TweenMax.to(".t02", 1.9, {
+    x: "-170",
+    y: "100",
+    scale: "1.8",
+    ease: Expo.easeInOut,
+    delay: 2.9,
+});
+
+TweenMax.to(".t0_mobile", 1.9, {
+    x: "-20",
+    y: "-40",
+    scale: "0.6",
+    ease: Expo.easeInOut,
+    delay: 2.9,
+});
+TweenMax.to(".t02_mobile", 1.9, {
+    x: "-70",
+    y: "10",
+    scale: "1.5",
+    ease: Expo.easeInOut,
+    delay: 2.9,
+});
+
+TweenMax.to(".t0_tab", 1.9, {
+    x: "-60",
+    y: "-5",
+    scale: "0.6",
+    ease: Expo.easeInOut,
+    delay: 2.9,
+});
+TweenMax.to(".t02_tab", 1.9, {
+    x: "-115",
+    y: "100",
+    scale: "1.6",
+    ease: Expo.easeInOut,
+    delay: 2.9,
+});
 
 
 
