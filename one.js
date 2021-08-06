d4401ddc4fae29f737824cf3c886e324fdc78082
Loic91 <62215559+Loic91@users.noreply.github.com>
2021-08-07 01:42:52 +0200

    Add files via upload

---
+++ one.js
@@ -0,0 +1,15 @@
+// OVERLAY
+TweenMax.to(".first", 3.8, {
+    delay: 0.1,
+    opacity: 0,
+    top: "-200%",
+    ease: Expo.easeInOut
+});
+
+
+
+
+gsap.from('#title__h1', 1.9, {
+    y: '100%',
+    delay: 2.5
+})
