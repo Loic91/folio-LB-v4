10e48813ddd363b4cd25f4262d6d715da2b34f40
Loic91 <62215559+Loic91@users.noreply.github.com>
2021-08-09 18:56:48 +0200

    Add files via upload

---
+++ passion.js
@@ -0,0 +1,43 @@
+gsap.from('#title__h1', 1.9, {
+    y: '100%',
+    delay: 1.9
+})
+gsap.from('#title__h1-2', 1.9, {
+    y: '100%',
+    delay: 2.5
+})
+
+gsap.from('.video', 2.5, {
+    opacity: 0,
+    y: '40%',
+    ease: Expo.easeInOut,
+    delay: 2.9,
+})
+
+
+
+gsap.from('.one', 0.9, {
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: 0.3,
+})
+gsap.from('.two', 0.9, {
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: 0.5,
+})
+gsap.from('.three', 0.9, {
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: 0.7,
+})
+gsap.from('.four', 0.9, {
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: 0.9,
+})
+gsap.from('.five', 0.9, {
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: 1.1,
+})
