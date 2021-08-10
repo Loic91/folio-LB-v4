9f181806de16a438ab25547c0dcdd7c0fe87e524
Loic91 <62215559+Loic91@users.noreply.github.com>
2021-08-10 16:58:22 +0200

    Add files via upload

--- passion.js
+++ passion.js
@@ -1,7 +1,16 @@
+// OVERLAY
+TweenMax.to(".overlay__one", 3.8, {
+    delay: 0.1,
+    opacity: 0,
+    top: "-200%",
+    ease: Expo.easeInOut
+});
+
+
 gsap.from('.navbar', 2.5, {
     opacity: 0,
     y: '-40%',
-    delay: 2.7,
+    delay: 2.9,
 })
 
 gsap.from('#title__h1', 1.9, {
@@ -43,21 +52,21 @@ gsap.from('.video', 2.5, {
 
 gsap.from('.one', 3.9, {
     opacity: 0,
-    delay: 0.3,
+    delay: 0.6,
 })
 gsap.from('.two', 3.9, {
     opacity: 0,
-    delay: 0.5,
+    delay: 0.8,
 })
 gsap.from('.three', 3.9, {
     opacity: 0,
-    delay: 0.7,
+    delay: 1,
 })
 gsap.from('.four', 3.9, {
     opacity: 0,
-    delay: 0.9,
+    delay: 1.2,
 })
 gsap.from('.five', 3.9, {
     opacity: 0,
-    delay: 1.1,
+    delay: 1.4,
 })
